d23da5a4a57dadfbbb3d0feab7b6e5d91502c386
Jean-Francis Bastien <bhacaz@gmail.com>
2023-06-21 21:31:30 -0400

    progress bar

--- src/views/Moon.vue
+++ src/views/Moon.vue
@@ -6,6 +6,8 @@ export default {
   data() {
     return {
       selectedTime: '',
+      difference: 0,
+      pourcentageRemaining: 0,
     };
   },
   mounted() {
@@ -14,12 +16,12 @@ export default {
       this.selectedTime = sleepTime;
     }
     this.startTimer();
+    setInterval(() => {
+      this.calculateRemaining();
+    }, 1000 * 60);
   },
   methods: {
-    goToSun() {
-      this.$router.push('/');
-    },
-    startTimer() {
+    wakeUpTime() {
       const sleepTime = new Date();
       const [hours, minutes] = this.selectedTime.split(':');
       sleepTime.setHours(hours);
@@ -30,19 +32,32 @@ export default {
       if (sleepTime.getHours() < now.getHours()) {
         sleepTime.setHours(sleepTime.getHours() + 24);
       }
-      const difference = sleepTime.getTime() - now.getTime();
+      return sleepTime;
+    },
+    goToSun() {
+      this.$router.push('/');
+    },
+    startTimer() {
+      const sleepTime = this.wakeUpTime();
+      const now = new Date();
+      this.difference = sleepTime.getTime() - now.getTime();
       // print how many hours and minutes until sleep time
-      const hoursUntilSleep = Math.floor(difference / 1000 / 60 / 60);
-      const minutesUntilSleep = Math.floor(difference / 1000 / 60) - (hoursUntilSleep * 60);
+      const hoursUntilSleep = Math.floor(this.difference / 1000 / 60 / 60);
+      const minutesUntilSleep = Math.floor(this.difference / 1000 / 60) - (hoursUntilSleep * 60);
       console.log(`Sleeping in ${hoursUntilSleep} hours and ${minutesUntilSleep} minutes`);
       setTimeout(() => {
         this.$refs.moonSection.classList.add('moon-transition-to-sun');
+        this.$refs.progressBar.classList.add('is-hidden');
         // Wait for transition to finish
         setTimeout(() => {
           this.goToSun()
         }, 9000);
-      }, difference);
+      }, this.difference);
     },
+    calculateRemaining() {
+      const now = new Date();
+      this.pourcentageRemaining = ((this.difference - (this.wakeUpTime().getTime() - now.getTime())) / this.difference * 100).toFixed(0);
+    }
   }
 }
 </script>
@@ -59,7 +74,7 @@ export default {
         <div class="block is-vcentered has-text-centered p-6">
           <div class="field is-grouped is-justify-content-center">
             <p class="control">
-              <button class="button is-outlined has-text-grey is-large is-rounded has-background-grey-darker" @click="goToSun()">
+              <button class="button is-outlined has-text-grey is-large is-rounded invisible-background" @click="goToSun()">
                 <span class="icon is-large">
                   <ion-icon name="close-outline"></ion-icon>
                 </span>
@@ -72,12 +87,17 @@ export default {
         </div>
       </div>
     </div>
+    <progress ref="progressBar" class="progress is-small" :value="pourcentageRemaining" max="100"></progress>
   </section>
 </template>
 
-<style>
+<style scoped>
 .moon-section {
   background-color: #0d1221;
+}
+
+.invisible-background {
+  background-color: transparent;
 }
 
 .moon-transition-to-sun {
@@ -88,6 +108,18 @@ export default {
 
 .moon {
   color: #3a4861;
+}
+
+.progress {
+  border: 1px solid #3a4861;
+}
+
+.progress::-webkit-progress-bar {
+  background-color: #0d1221;
+}
+
+.progress::-webkit-progress-value {
+  background-color: #3a4861;
 }
 
 @keyframes colorTransition {
--- src/views/Sunny.vue
+++ src/views/Sunny.vue
@@ -64,7 +64,7 @@ export default {
   </section>
 </template>
 
-<style>
+<style scoped>
 .sun-section {
   background-color: #ffffcc;
 }
