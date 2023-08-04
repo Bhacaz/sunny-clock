9e214b2b09e87e9592f3716b2fcbf82dd75ef454
Jean-Francis Bastien <bhacaz@gmail.com>
2023-08-04 13:34:46 -0400

    PWA workbox cache assets

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,59 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+      vue(),
+      VitePWA({
+        registerType: 'autoUpdate',
+        strategies: 'generateSW',
+        devOptions: {
+          enabled: true
+        },
+          // add this to cache all the imports
+          workbox: {
+              globPatterns: ["**/*"],
+          },
+          // add this to cache all the
+          // static assets in the public folder
+          includeAssets: [
+              "**/*",
+          ],
+          manifest: {
+            "theme_color": "#efef6a",
+            "background_color": "#efef6a",
+            "display": "fullscreen",
+            "name": "Sunny Clock",
+            "description": "Sunny Clock",
+            "short_name": "Sunny Clock",
+            "icons": [
+                {
+                    "src": "/icon-192x192.png",
+                    "sizes": "192x192",
+                    "type": "image/png"
+                },
+                {
+                    "src": "/icon-256x256.png",
+                    "sizes": "256x256",
+                    "type": "image/png"
+                },
+                {
+                    "src": "/icon-384x384.png",
+                    "sizes": "384x384",
+                    "type": "image/png"
+                },
+                {
+                    "src": "/icon-512x512.png",
+                    "sizes": "512x512",
+                    "type": "image/png"
+                }
+            ]
+        }
+      })
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
